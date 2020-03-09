c15a9d1d549de71e4680e207cf175b88d4b10578
Anran Qin <51769728+ALeafWolf@users.noreply.github.com>
2020-03-09 12:25:25 -0400

    create Home.js

---
+++ src/screens/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+
+const style = StyleSheet.create({
+  container: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
